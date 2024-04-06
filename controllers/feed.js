00cf83e4fea204351da3323a5f0a6af587796b33
MehmetKaldirim <mathdenizi@gmail.com>
2024-04-06 22:11:29 +0200

    mongodb connection work properly

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,7 @@
+const expValidator = require("express-validator");
+
+const Post = require("../models/post");
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
@@ -16,17 +20,31 @@ exports.getPosts = (req, res, next) => {
 };
 
 exports.createPost = (req, res, next) => {
+  const errors = expValidator.validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      message: "Validation failed, entered data is incorrect",
+      errors: errors.array,
+    });
+  }
   const title = req.body.title;
   const content = req.body.content;
-  // Create post in db
-  res.status(201).json({
-    message: "Post created successfully!",
-    post: {
-      _id: new Date().toISOString(),
-      title: title,
-      content: content,
-      creator: {name: 'math'},
-      createdAt: new Date(),
-    },
+  const post = new Post({
+    title: title,
+    content: content,
+    imageUrl: 'images/duck.png',
+    creator: { name: "math" },
   });
+  post
+    .save()
+    .then((result) => {
+      console.log(result);
+      res.status(201).json({
+        message: "Post created successfully!",
+        post: result
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
