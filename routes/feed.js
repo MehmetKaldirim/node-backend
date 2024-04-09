ea21b148a089e7fd97630f2cc4779600eb330a7c
MehmetKaldirim <mathdenizi@gmail.com>
2024-04-09 06:15:56 +0200

    authentication added

--- routes/feed.js
+++ routes/feed.js
@@ -2,15 +2,16 @@ const express = require("express");
 const expValidator = require("express-validator");
 
 const feedController = require("../controllers/feed");
+const isAuth = require("../middleware/is-auth");
 
 const router = express.Router();
 
 // GET /feed/posts
-router.get("/posts", feedController.getPosts);
+router.get("/posts", isAuth, feedController.getPosts);
 
 // POST /feed/post
 router.post(
-  "/post",
+  "/post",isAuth,
   [
     expValidator.body("title").trim().isLength({ min: 5 }),
     expValidator.body("content").trim().isLength({ min: 5 }),
@@ -18,9 +19,9 @@ router.post(
   feedController.createPost
 );
 
-router.get("/post/:postId", feedController.getPost);
+router.get("/post/:postId",isAuth, feedController.getPost);
 router.put(
-  "/post/:postId",
+  "/post/:postId",isAuth,
   [
     expValidator.body("title").trim().isLength({ min: 5 }),
     expValidator.body("content").trim().isLength({ min: 5 }),
@@ -28,5 +29,5 @@ router.put(
   feedController.updatePost
 );
 
-router.delete("/post/:postId", feedController.deletePost);
+router.delete("/post/:postId",isAuth, feedController.deletePost);
 module.exports = router;
